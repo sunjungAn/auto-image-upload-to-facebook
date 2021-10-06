400484ee24458d3ed7c4c9ff171f8a6bb2e0416f
SunjungAn <55094745+sunjungAn@users.noreply.github.com>
2021-10-06 23:49:08 +0900

    Update test01.php

--- test01.php
+++ test01.php
@@ -1,8 +1,8 @@
 <?php 
 
 $fb = new Facebook\Facebook([
-  'app_id' => '428247532338328',
-  'app_secret' => '31f27495e0f97411f10571624ac8e280',
+  'app_id' => '',
+  'app_secret' => '',
   'default_graph_version' => 'v2.10',
   ]);
 
